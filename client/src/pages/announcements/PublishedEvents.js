cd049a00ac7c4ce5c6477b0f118ddb0f288a9942
Ben Koo <koo0905@gmail.com>
2023-05-04 13:35:54 +0800

    Added the ability to see time

--- client/src/pages/announcements/PublishedEvents.js
+++ client/src/pages/announcements/PublishedEvents.js
@@ -1,4 +1,5 @@
 import {ethers} from 'ethers'
+
 // import CS_ABI from '../../features/blockchain/ChangeSuggestion_abi.json'
 // import SS_ABI from '../../features/blockchain/SimpleStore_abi.json'
 import AC_ABI from '../../features/blockchain/AccountableChange_abi.json'
@@ -7,7 +8,8 @@ import {Link} from 'react-router-dom'
 
 const PublishedEvents = () => {
   
-  const ETHERSCAN_PREFIX = "https://goerli.etherscan.io/address/"
+  const ETHER_NETWORK = process.env.REACT_APP_ETHEREUM_NETWORK
+  const ETHERSCAN_PREFIX = `${ETHER_NETWORK}` ? `https://${ETHER_NETWORK}.etherscan.io/` : "https://etherscan.io/"
   const CONTRACT_ADDRESS = process.env.REACT_APP_ACCOUNTABLE_CHANGE_CONTRACT_ADDRESS
   const eventName = "ChangeSubmitted"
   
@@ -44,22 +46,42 @@ const PublishedEvents = () => {
     const fetchedEvents = await contract.queryFilter(eventName, 0, currentBlkNum);
     setEvents(fetchedEvents)
   }
+   
+  function getEventTypes(eventName) {
+
+    const event = AC_ABI.find(item => item.type === 'event' && item.name === eventName);
+    if (!event) {
+      console.log("No event found for event");
+      throw new Error(`Event ${eventName} not found in contract ABI`);
+    }
+    const eventTypes = event.inputs.map(input => input.type);
+    return eventTypes;
+  }
 
   const queryFilterWithEtherscan = async () => {
     const aProvider = new ethers.providers.EtherscanProvider("goerli", process.env.REACT_APP_ETHERSCAN_KEY);
     setProvider(aProvider);
     setProviderName("Etherscan:" + aProvider.getBaseUrl());
+    const eventTypes = getEventTypes(eventName);
+
+    const eventInterface = new ethers.utils.Interface([`event ${eventName}(${eventTypes.join(',')})`]);
+    const eventSignature = eventInterface.getEventTopic(eventName);
+    console.log("eventSignature:" + JSON.stringify(eventSignature))
 
     const url =  aProvider.getBaseUrl()
     console.log("Use Etherscan Provider URL:" + JSON.stringify(url));
     let currentBlkNum = await aProvider.getBlockNumber();
     console.log("Current Block Number" + currentBlkNum);
+    const block = await provider.getBlock(currentBlkNum);
+    const timestamp = block.timestamp;
+    const date = new Date(timestamp * 1000);
+    console.log("Current Block's Timestamp:" + date.toLocaleString())
+
 
     const contract = await new ethers.Contract(CONTRACT_ADDRESS, AC_ABI, aProvider)
 
     const fetchedEvents = await contract.queryFilter(eventName, 0, currentBlkNum);
-    const oneEvent = fetchedEvents[0];
-    console.log("blockTimestamp:" + oneEvent.blockTimestamp)
+    console.log("fetchedEvents:" + JSON.stringify(fetchedEvents))
     setEvents(fetchedEvents.reverse())
   }
 
@@ -67,9 +89,10 @@ const PublishedEvents = () => {
     const fetchData = async () => {
       const rows = await Promise.all(
         events.map(async (event) => {
-          const proposer =  event.args.sender;
-          const data =  event.args.data;
-          return { proposer, data };
+          const proposer =  event.args.sender
+          const data =  event.args.data
+          const blockNumber = event?.blockNumber
+          return { proposer, data , blockNumber};
         })
       );
       setTableData(rows);
@@ -90,12 +113,15 @@ const PublishedEvents = () => {
         <p>Event List Table</p>
         <p>From {providerName}</p>
         <table>
-          <th>Change Submission Account </th><th>   Change Instruction</th>
+        <th>Time: ( Larger number is later)</th> <th>Change Submission Account </th><th>   Change Instruction</th>
         {tableData.map( (entry) => (
-        <tr>
-          <td><Link to={`${ETHERSCAN_PREFIX}${entry?.proposer}`} target="_blank" rel="noopener noreferrer">{entry?.proposer}</Link></td>
-          <td>{entry?.data}</td>
-        </tr>))}
+          <tbody key={crypto.randomUUID()}>
+          <tr key={crypto.randomUUID()}>
+            <td><Link to={`${ETHERSCAN_PREFIX}/block/${entry?.blockNumber}`} target="_blank" rel="noopener noreferrer">{entry?.blockNumber}</Link></td>
+            <td><Link to={`${ETHERSCAN_PREFIX}/address/${entry?.proposer}`} target="_blank" rel="noopener noreferrer">{entry?.proposer}</Link></td>
+            <td>{entry?.data}</td>
+          </tr>
+          </tbody>))}
         </table>
       </div>
   </div>
